7fefb946e20a40c3394570120507c6a1d5f3380e
reuben <reuben.vincent.reid@gmail.com>
2025-04-30 18:20:07 +0100

    completed problem for count complete subarrays

--- elixir/leets/lib/count_complete_subarrays.ex
+++ elixir/leets/lib/count_complete_subarrays.ex
@@ -2,25 +2,48 @@ defmodule Solution do
   @spec count_complete_subarrays(nums :: [integer]) :: [integer]
   def count_complete_subarrays(nums) do
     check_for_complete_subarrays(nums)
-    |> Enum.filter(fn x -> x == true end)
-    |> length()
   end
 
-  @spec is_complete_subarray?(subarray :: [integer], unique_nums_count :: [integer]) :: boolean
-  defp is_complete_subarray?(subarray, unique_nums_count) do
-    length(Enum.uniq(subarray)) == unique_nums_count
+  def done_sliding(acc), do: acc
+
+  def slide_window(nums, num_set, left_pointer, right_pointer, freq, acc) do
+    value = Enum.at(nums, right_pointer)
+
+    if right_pointer < length(nums) && map_size(freq) < num_set do
+      freq = Map.update(freq, value, 1, &(&1 + 1))
+      slide_window(nums, num_set, left_pointer, right_pointer + 1, freq, acc)
+    else
+      value = Enum.at(nums, left_pointer)
+
+      acc =
+        cond do
+          map_size(freq) == num_set ->
+            acc + length(nums) - right_pointer + 1
+
+          true ->
+            acc
+        end
+
+      freq =
+        case Map.get(freq, value) do
+          1 ->
+            Map.delete(freq, value)
+
+          _ ->
+            Map.update(freq, value, nil, &(&1 - 1))
+        end
+
+      if left_pointer < length(nums) do
+        slide_window(nums, num_set, left_pointer + 1, right_pointer, freq, acc)
+      else
+        done_sliding(acc)
+      end
+    end
   end
 
   def check_for_complete_subarrays(nums) do
-    nums_length = length(nums)
-    unique_nums_count = length(Enum.uniq(nums))
+    num_set = length(Enum.uniq(nums))
 
-    Enum.flat_map(0..(nums_length - 1), fn n ->
-      Enum.map(n..(nums_length - 1), fn i ->
-        Enum.drop(nums, n)
-        |> Enum.take(i - n + 1)
-        |> is_complete_subarray?(unique_nums_count)
-      end)
-    end)
+    slide_window(nums, num_set, 0, 0, %{}, 0)
   end
 end
